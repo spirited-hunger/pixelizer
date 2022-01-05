08f799fac719c44a21dd7dde48669d2d514797ce
Spirited Hunger <sjkang920921@gmail.com>
2022-01-05 23:49:27 +0900

    pixel perfect canvas

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,15 @@
+var App = /** @class */ (function () {
+    function App() {
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+        console.log(this);
+    }
+    return App;
+}());
+window.onload = function () {
+    new App();
+    console.log(devicePixelRatio);
+};
 //# sourceMappingURL=main.js.map
