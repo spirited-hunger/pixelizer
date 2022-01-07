2613e76574b03b93b834ac46e02a3a65f4134717
Spirited Hunger <sjkang920921@gmail.com>
2022-01-08 00:59:06 +0900

    base64

--- dist/test/particleRain.js
+++ dist/test/particleRain.js
@@ -1,5 +1,13 @@
-import {astronaut} from "./images.js";
-
-const myImage = new Image();
-
-myImage.src = astronaut
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var images_js_1 = require("./images.js");
+var myImage = new Image();
+myImage.src = images_js_1.images.astronaut;
+myImage.addEventListener('load', function () {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    canvas.width = 500;
+    canvas.height = 700;
+    ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+});
+//# sourceMappingURL=particleRain.js.map
---
+++ src/test/particleRain.ts
@@ -0,0 +1,16 @@
+import {images} from "./images.js";
+
+const myImage = new Image();
+myImage.src = images.astronaut;
+
+myImage.addEventListener('load', () => { // after all, base64 is a url request. sometimes it responds slower than js file. so it fails to upload the image all the time. this is why we add eventlistener
+  const canvas: any = document.getElementById("myCanvas");
+
+  const ctx = canvas.getContext("2d");
+  
+  canvas.width = 500;
+  canvas.height = 700;
+  
+  ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+  
+})
