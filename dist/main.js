fccd68c91e1a524a087fe96c236b4f8ec8e8f6a0
Spirited Hunger <sjkang920921@gmail.com>
2022-01-08 14:06:56 +0900

    main css

--- dist/main.js
+++ dist/main.js
@@ -1,27 +1,29 @@
-var App = /** @class */ (function () {
-    function App() {
-        this.canvas = document.createElement('canvas');
-        document.body.appendChild(this.canvas);
-        this.ctx = this.canvas.getContext('2d');
-        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
-        window.addEventListener('resize', this.resize.bind(this), false);
-        this.resize();
-        window.requestAnimationFrame(this.animate.bind(this));
-    }
-    App.prototype.resize = function () {
-        this.stageWidth = document.body.clientWidth;
-        this.stageHeight = document.body.clientWidth;
-        this.canvas.width = this.stageWidth * this.pixelRatio;
-        this.canvas.height = this.stageHeight * this.pixelRatio;
-    };
-    ;
-    App.prototype.animate = function () {
-    };
-    ;
-    return App;
-}());
-window.onload = function () {
-    new App();
-    // console.log(devicePixelRatio)
-};
+// class App {
+//   canvas: any;
+//   ctx: any;
+//   pixelRatio: number;
+//   stageWidth: number;
+//   stageHeight: number;
+//   resize() {
+//     this.stageWidth = document.body.clientWidth;
+//     this.stageHeight = document.body.clientWidth;
+//     this.canvas.width = this.stageWidth * this.pixelRatio;
+//     this.canvas.height = this.stageHeight * this.pixelRatio;
+//   };
+//   animate() {
+//   };
+//   constructor() {
+//     this.canvas = document.createElement('canvas');
+//     document.body.appendChild(this.canvas);
+//     this.ctx = this.canvas.getContext('2d');
+//     this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+//     window.addEventListener('resize', this.resize.bind(this), false);
+//     this.resize();
+//     window.requestAnimationFrame(this.animate.bind(this));
+//   }
+// }
+// window.onload = () => {
+//   new App();
+//   // console.log(devicePixelRatio)
+// }
 //# sourceMappingURL=main.js.map
--- src/test/particleRain.ts
+++ src/test/particleRain.ts
@@ -13,4 +13,56 @@ myImage.addEventListener('load', () => { // after all, base64 is a url request. 
   
   ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
   
+
+  let particlesArray = [];
+  const numberOfParticles = 5000;
+
+  class Particle {
+      x: number;
+      y: number;
+      speed: number;
+      velocity: number;
+      size: number;
+
+      constructor() {
+          this.x = Math.random() * canvas.width;
+          this.y = 0;
+          this.speed = 0;
+          this.velocity = Math.random() * 3.5;
+          this.size = Math.random() * 1.5 + 1;
+      }
+
+      update() {
+        this.y += this.velocity;
+        if (this.y >= canvas.height) {
+          this.y = 0;
+          this.x = Math.random() * canvas.width;
+        }
+      }
+
+      draw() {
+        ctx.beginPath();
+        ctx.fillStyle = 'white';
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+      }
+  }
+  function init() {
+    for (let i = 0; i < numberOfParticles; i ++) {
+      particlesArray.push(new Particle);
+    }
+  }
+  init();
+
+  function animate () {
+    ctx.globalAlpha = 0.05;
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < particlesArray.length; i++) {
+      particlesArray[i].update();
+      particlesArray[i].draw();
+    }
+    requestAnimationFrame(animate);
+  }
+  animate();
 })
