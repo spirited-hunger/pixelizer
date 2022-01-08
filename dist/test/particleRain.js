2ad4c3cfacf8e967ab2b05a36bb7c8692c6dce38
Spirited Hunger <sjkang920921@gmail.com>
2022-01-08 14:06:56 +0900

    main css

--- dist/test/particleRain.js
+++ dist/test/particleRain.js
@@ -7,5 +7,47 @@ myImage.addEventListener('load', function () {
     canvas.width = 500;
     canvas.height = 500;
     ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+    var particlesArray = [];
+    var numberOfParticles = 5000;
+    var Particle = /** @class */ (function () {
+        function Particle() {
+            this.x = Math.random() * canvas.width;
+            this.y = 0;
+            this.speed = 0;
+            this.velocity = Math.random() * 3.5;
+            this.size = Math.random() * 1.5 + 1;
+        }
+        Particle.prototype.update = function () {
+            this.y += this.velocity;
+            if (this.y >= canvas.height) {
+                this.y = 0;
+                this.x = Math.random() * canvas.width;
+            }
+        };
+        Particle.prototype.draw = function () {
+            ctx.beginPath();
+            ctx.fillStyle = 'white';
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+            ctx.fill();
+        };
+        return Particle;
+    }());
+    function init() {
+        for (var i = 0; i < numberOfParticles; i++) {
+            particlesArray.push(new Particle);
+        }
+    }
+    init();
+    function animate() {
+        ctx.globalAlpha = 0.05;
+        ctx.fillStyle = 'rgb(0, 0, 0)';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        for (var i = 0; i < particlesArray.length; i++) {
+            particlesArray[i].update();
+            particlesArray[i].draw();
+        }
+        requestAnimationFrame(animate);
+    }
+    animate();
 });
 //# sourceMappingURL=particleRain.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,40 @@
-class App {
-  canvas: any;
-  ctx: any;
-  pixelRatio: number;
-  stageWidth: number;
-  stageHeight: number;
+// class App {
+//   canvas: any;
+//   ctx: any;
+//   pixelRatio: number;
+//   stageWidth: number;
+//   stageHeight: number;
 
-  resize() {
-    this.stageWidth = document.body.clientWidth;
-    this.stageHeight = document.body.clientWidth;
+//   resize() {
+//     this.stageWidth = document.body.clientWidth;
+//     this.stageHeight = document.body.clientWidth;
 
-    this.canvas.width = this.stageWidth * this.pixelRatio;
-    this.canvas.height = this.stageHeight * this.pixelRatio;
-  };
+//     this.canvas.width = this.stageWidth * this.pixelRatio;
+//     this.canvas.height = this.stageHeight * this.pixelRatio;
+//   };
 
-  animate() {
+//   animate() {
 
-  };
+//   };
 
-  constructor() {
-    this.canvas = document.createElement('canvas');
-    document.body.appendChild(this.canvas);
-    this.ctx = this.canvas.getContext('2d');
+//   constructor() {
+//     this.canvas = document.createElement('canvas');
+//     document.body.appendChild(this.canvas);
+//     this.ctx = this.canvas.getContext('2d');
 
-    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+//     this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
 
-    window.addEventListener('resize', this.resize.bind(this), false);
+//     window.addEventListener('resize', this.resize.bind(this), false);
 
-    this.resize();
+//     this.resize();
 
-    window.requestAnimationFrame(this.animate.bind(this));
-  }
-}
+//     window.requestAnimationFrame(this.animate.bind(this));
+//   }
+// }
 
-window.onload = () => {
-  new App();
-  // console.log(devicePixelRatio)
-}
+// window.onload = () => {
+//   new App();
+//   // console.log(devicePixelRatio)
+// }
+
+
