8ed74b42e7502e0ae7afb10cdc71a39b86f850d3
Spirited Hunger <sungjk511@gmail.com>
2022-01-21 23:21:34 +0900

    tidying up

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,8 @@
 //   // console.log(devicePixelRatio)
 // }
 
-/* important values */
+/* V A L U E S */
+
 const CANVAS_MAX_WIDTH : number = 1080;
 const CANVAS_MAX_HEIGHT : number = 1080;
 
@@ -61,7 +62,7 @@ let imageHeight: number;
 
 let colorFromImage = "linear-gradient(180deg, rgb(227,227,227) 0%, rgba(188,195,205) 100%)";
 
-/* selecting all required elements */
+/* H T M L  E L E M E N T S */
 
 /* drop area */
 const dropArea: HTMLElement = document.querySelector(".drag-area");
@@ -75,8 +76,6 @@ const input: HTMLInputElement = dropArea.querySelector("input");
 /* image area */
 const imgArea: HTMLElement = document.querySelector(".image-area");
 
-const originalImage: HTMLImageElement = new Image();
-
 const imgCanvas: HTMLCanvasElement = document.createElement('canvas');
 const imgContext: any = imgCanvas.getContext('2d');
 
@@ -86,20 +85,48 @@ const pixContext: any = pixCanvas.getContext('2d');
 /* palette area */
 const paletteArea: HTMLDivElement = document.querySelector(".palette-area");
 
-let imgFile : any;
+/* C O M M A N D S */
 
-browseButton.onclick = () => {
+/* D R A G , D R O P & B R O W S E  F I L E */
+
+let imgFile : File;
+
+browseButton.addEventListener("click", () => {
   input.click(); // if browseButton is clicked the input is also clicked
-}
+});
 
-pixelateButton.onclick = () => {
+input.addEventListener("change", (e: Event) : void => {
+  const input = (e.target as HTMLInputElement)
+
+  // getting user select file and [0] means if a user selets multiple files we'll select only the first one
+  imgFile = input.files[0];
+  // ! if use this.files[0], "this" points to window because it is an arrow function
+  showFile();
+})
+
+// if user drag file over image area
+dropArea.addEventListener("dragover", (e: Event) : void => showReleaseMsg(e));
+
+// if user leave dragged file from image area
+dropArea.addEventListener("dragleave", () => showUploadMsg())
+
+// if user drop file on drag area
+dropArea.addEventListener("drop", (e: any) : void => {
+  e.preventDefault(); // preventing from default behaviour (opening a new tab)
+  // console.log("File is dropped on drop area");
+
+  // getting user select file and [0] means if a user selets multiple files we'll select only the first one
+  imgFile = e.dataTransfer.files[0];
+  // console.log(imgFile);
+  showFile();
+})
+
+/* P I X A L A T E */
+
+pixelateButton.addEventListener("click", () => {
   if (imageWidth !== undefined && imageHeight !== undefined) { // double-check if image is uploaded
-    // TODO : consider deleting this console log
+    // TODO : consider deleting this console.log
     console.log("pix!");
-
-    // deleting image with black
-    imgContext.fillStyle = "black";
-    imgContext.fillRect(0, 0, imgCanvas.width, imgCanvas.height);
 
     const pixelSize: number = PIXEL_SIZE;
     const pixelNumCol: number = Math.floor(imageWidth / pixelSize);
@@ -109,7 +136,12 @@ pixelateButton.onclick = () => {
     pixCanvas.width = pixelNumCol;
     pixCanvas.height = pixelNumRow;
 
-    pixContext.drawImage(originalImage, 0, 0, pixelNumCol, pixelNumRow);
+    pixContext.drawImage(imgCanvas, 0, 0, pixelNumCol, pixelNumRow);
+
+    // deleting image with black
+    imgContext.fillStyle = "black";
+    imgContext.fillRect(0, 0, imgCanvas.width, imgCanvas.height);
+
     /* getting pixelated data */
     const pixData = pixContext.getImageData(0, 0, pixelNumCol, pixelNumRow).data;
 
@@ -154,39 +186,20 @@ pixelateButton.onclick = () => {
   }
 
   showPalette();
-}
+});
 
-input.addEventListener("change", (e:Event) : void => {
-  const input = (e.target as HTMLInputElement)
-
-  // getting user select file and [0] means if a user selets multiple files we'll select only the first one
-  imgFile = input.files[0];
-  // ! if use this.files[0], "this" points to window because it is an arrow function
-  showFile();
-})
-
-// if user drag file over image area
-dropArea.addEventListener("dragover", (e) => showReleaseMsg(e));
-
-// if user leave dragged file from image area
-dropArea.addEventListener("dragleave", () => showUploadMsg())
-
-// if user drop file on drag area
-dropArea.addEventListener("drop", (event : any) : void => {
-  event.preventDefault(); // preventing from default behaviour (opening a new tab)
-  // console.log("File is dropped on drop area");
-
-  // getting user select file and [0] means if a user selets multiple files we'll select only the first one
-  imgFile = event.dataTransfer.files[0];
-  // console.log(imgFile);
-  showFile();
-})
-
-const showFile = () => {
+// function to 
+const showFile = (
+  // fromImgFile: File, 
+  // toDivEl: HTMLDivElement, 
+  // toCanvasEl: HTMLCanvasElement
+) => {
   let fileType = imgFile.type;
   let validExtentsions: string[] = ["image/jpeg", "image/jpg", "image/png"];
 
   if (validExtentsions.includes(fileType)) {
+    const originalImage: HTMLImageElement = new Image();
+
     let fileReader: FileReader = new FileReader();
     
     fileReader.readAsDataURL(imgFile);
@@ -220,9 +233,9 @@ const showFile = () => {
         imgCanvas.height = imageHeight;
 
         /* fit canvas in screen */
-        // TODO : 마진 값 확인하기 (진짜 1080 픽셀이 맞는지)
+        // TODO : 나중에 마진 값 확인하기 (진짜 1080 픽셀이 맞는지)
         const imageCSSWH = Number(window.getComputedStyle(imgCanvas).width.split("px")[0]);
-        console.log(imageCSSWH)
+        
         imgCanvas.style.width = `${
           Math.floor(imageWidth * imageCSSWH / CANVAS_MAX_WIDTH)
         }px`;
