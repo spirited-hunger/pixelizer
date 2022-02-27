7ef7d5969ec65898380dcf89df8507720ef6929f
Spirited Hunger <sungjk511@gmail.com>
2022-02-27 12:01:26 +0900

    add react router v6

--- pixpix-react/src/index.tsx
+++ pixpix-react/src/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './components/home_components/Home';
+import Editor from './components/editor_components/Editor';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import './App.css'
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/editor" element={<Editor />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +26,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
