7d94a823682a734886fc19f20d5e3e8dce9ed24f
Spirited Hunger <sungjk511@gmail.com>
2022-03-03 22:58:00 +0900

    edit home tailwind

--- pixpix-react/src/components/Home.tsx
+++ pixpix-react/src/components/Home.tsx
@@ -25,8 +25,8 @@ function Home() {
     </div>
     <div className="preview-area bg-gradient-to-b from-blue-grey to-smooth-grey flex justify-center items-center overflow-x-auto overflow-y-auto">
       <div className="image-area h-[400px] flex-400 flex flex-col">
-        <div className="image-bar flex-[0_0_30px] border-lineWidth border-dark-line"></div>
-        <div className="image-content border-l-lineWidth border-r-lineWidth border-b-lineWidth border-dark-line flex-[1_0_auto]">image content</div>
+        <div className="image-bar box-border flex-[0_0_30px] border-lineWidth border-dark-line bg-light-grey"></div>
+        <div className="image-content box-border border-l-lineWidth border-r-lineWidth border-b-lineWidth border-dark-line flex-[1_0_auto]">image content</div>
       </div>
     </div>
   </div>;
